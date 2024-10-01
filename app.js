4c1d2d03d130c4f692b567a5daba5cc359d397c0
sarabangroup <mongodbsaraban@gmail.com>
2024-10-02 01:32:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.use('/api', reviewRoutes);
 
 // Starting the server
 const PORT = process.env.PORT || 3001;
-app.listen(PORT,0.0.0.0/0, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
